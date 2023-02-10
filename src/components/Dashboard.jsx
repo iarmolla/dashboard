2f246c61a35a5b3f40ff72801b2c4cf4eb1881f0
iarmolla <ivan.armolla@gmail.com>
2023-02-10 19:09:35 -0300

    changing styles to dashboard

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -13,7 +13,7 @@ function Dashboard() {
   return (
     <>
       <Sidebar></Sidebar>
-      <nav className=" border-b-2 border-gray-200 dark:border-gray-700  px-2 sm:px-4 py-2.5 rounded ">
+      <nav className="border-b-2 border-gray-200 dark:border-gray-700 px-2 sm:px-4 py-2.5">
         <div className="container flex flex-wrap items-center justify-between mx-auto">
          <div></div>
           <div className="flex flex-col items-center md:order-2">
@@ -24,19 +24,13 @@ function Dashboard() {
 
             <div className={`z-50 ${user ? 'absolute' : 'hidden'} top-24 right-10  md:top-11 md:right-0 md:mr-1 my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600`} >
               <div className="px-4 py-3">
-                <span className="block text-sm text-gray-900 dark:text-white">Bonnie Green</span>
-                <span className="block text-sm font-medium text-gray-500 truncate dark:text-gray-400">name@flowbite.com</span>
+                <span className="block text-sm text-gray-900 dark:text-white">username</span>
+                <span className="block text-sm font-medium text-gray-500 truncate dark:text-gray-400">admin@gmail.com</span>
               </div>
-              <ul className="py-2" aria-labelledby="user-menu-button">
-                <li>
-                  <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Dashboard</a>
-                </li>
+              <ul className="py-2" aria-labelledby="user-menu-button">               
                 <li>
                   <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Settings</a>
-                </li>
-                <li>
-                  <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Earnings</a>
-                </li>
+                </li>               
                 <li>
                   <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Sign out</a>
                 </li>
