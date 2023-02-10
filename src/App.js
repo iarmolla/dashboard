c5f9198b446aa273ad6fe4c98100474039a46568
iarmolla <ivan.armolla@gmail.com>
2023-02-10 19:08:25 -0300

    adding login & register

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import "./App.css";
 import Dashboard from "./components/Dashboard";
+import Data from "./components/Data";
 import Users from "./components/Users";
 import List from './components/Table'
 import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
+import Login from "./components/Login";
+import Register from "./components/Register";
+
 
 function App() {
   return (
-    <div className="App dark:bg-gray-800 h-screen">
+    <div className="App h-screen">
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Dash />}>
-            <Route path="/create" element={<Users />}></Route>          
-            <Route path="/users" element={<List/>}></Route>
+            <Route path="/create" element={<Users />}></Route>
+            <Route path="/users" element={<List />}></Route>
+            <Route path="/data" element={<Data />}></Route>
           </Route>
+          <Route path="/login"element={<Login/>} ></Route>
+          <Route path="/register"element={<Register/>} ></Route>
+
         </Routes>
       </BrowserRouter>
     </div>
