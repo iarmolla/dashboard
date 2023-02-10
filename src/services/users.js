2192218eb91b7978b7d2fcb962a95d1df6e8254d
iarmolla <ivan.armolla@gmail.com>
2023-02-10 19:08:48 -0300

    adding endpoints login & register

--- src/services/users.js
+++ src/services/users.js
@@ -1,18 +1,36 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'https://crud-express-production.up.railway.app'
+  baseURL: 'http://localhost:3030'
 })
 
 export const getUsers = async () => {
-    const request = await api.get('/users')
-    return request.data
+  const request = await api.get('/users')
+  return request.data
 }
 
 export const createUser = async (user) => {
-  api.post('/users',user)
+  api.post('/users', user)
 }
 
 export const deleteUser = async (id) => {
   api.delete(`/users/${id}`)
+}
+
+export const editUser = async (user) => {
+  api.put(`/users`, user)
+}
+
+export const loginUser = async (user) => {
+  const token = localStorage.getItem('token')
+  const headers = {
+    "x-access-token": token
+  };
+  return await api.post(`/login`, user, {
+    headers: headers
+  })
+}
+
+export const registerUser = async (user) => {
+  return await api.post(`/register`, user)
 }
