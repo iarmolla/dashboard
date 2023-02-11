8feb0cb08debecb870272af4bf13e59ec60b2b49
iarmolla <ivan.armolla@gmail.com>
2023-02-11 18:16:54 -0300

    adding funcionality

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -2,49 +2,44 @@ import { useQuery } from 'react-query'
 import { getUsers } from '../services/users'
 import Sidebar from './Sidebar'
 import { useState } from 'react';
+import { signOut } from '../helpers/signout'
+import { useNavigate } from 'react-router-dom';
 
 function Dashboard() {
+  const navigate = useNavigate('')
   const [user, setUser] = useState(false)
   const { isLoading, data, isError, error } = useQuery({
     queryKey: ['users'],
     queryFn: getUsers
-  }) 
-
+  })
   return (
     <>
       <Sidebar></Sidebar>
       <nav className="border-b-2 border-gray-200 dark:border-gray-700 px-2 sm:px-4 py-2.5">
         <div className="container flex flex-wrap items-center justify-between mx-auto">
-         <div></div>
+          <div></div>
           <div className="flex flex-col items-center md:order-2">
             <button type="button" className="flex mr-3 text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" onClick={() => { setUser(!user) }} >
               <span className="sr-only">Open user menu</span>
               <img className="w-8 h-8 rounded-full" src="https://flowbite.com/docs/images/people/profile-picture-3.jpg" alt="user photo" />
             </button>
-
-            <div className={`z-50 ${user ? 'absolute' : 'hidden'} top-24 right-10  md:top-11 md:right-0 md:mr-1 my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600`} >
+            <div className={`z-50 ${user ? 'absolute' : 'hidden'} top-24 right-10  md:top-11 md:right-0 md:mr-1 my-4 text-base list-none btn-submit rounded-lg shadow `} >
               <div className="px-4 py-3">
-                <span className="block text-sm text-gray-900 dark:text-white">username</span>
-                <span className="block text-sm font-medium text-gray-500 truncate dark:text-gray-400">admin@gmail.com</span>
+                <span className="block text-sm font-medium truncate text-gray-400">{localStorage.getItem('email') || 'email'}</span>
               </div>
-              <ul className="py-2" aria-labelledby="user-menu-button">               
+              <ul className="py-2" aria-labelledby="user-menu-button">
                 <li>
-                  <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Settings</a>
-                </li>               
-                <li>
-                  <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Sign out</a>
+                  <span onClick={() => signOut(navigate)} className="link block px-4 py-2 text-sm text-gray-400">Sign out</span>
                 </li>
               </ul>
             </div>
 
           </div>
           <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="mobile-menu-2">
-            
+
           </div>
         </div>
       </nav>
-
-
     </>
   )
 }
