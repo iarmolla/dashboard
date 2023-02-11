103154254e20cd16afda037c0c8dd222900fa693
iarmolla <ivan.armolla@gmail.com>
2023-02-11 18:16:29 -0300

    adding protected routes

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import List from './components/Table'
 import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
 import Login from "./components/Login";
 import Register from "./components/Register";
+import ProtectedRoute from "./components/ProtectedRoute";
+import Error from "./components/Error";
 
 
 function App() {
@@ -13,14 +15,16 @@ function App() {
     <div className="App h-screen">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Dash />}>
+          <Route path="/" element={<ProtectedRoute>
+            <Dash />
+          </ProtectedRoute>}>
             <Route path="/create" element={<Users />}></Route>
-            <Route path="/users" element={<List />}></Route>
+            <Route path="/" element={<List />}></Route>
             <Route path="/data" element={<Data />}></Route>
           </Route>
           <Route path="/login"element={<Login/>} ></Route>
           <Route path="/register"element={<Register/>} ></Route>
-
+          <Route path="*" element={<Error/>}></Route>
         </Routes>
       </BrowserRouter>
     </div>
