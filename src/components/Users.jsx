3074e383fa6d737de3f544c99398c2fd6d2b15e2
iarmolla <ivan.armolla@gmail.com>
2023-02-11 18:20:41 -0300

    adding validations & error message

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -5,29 +5,42 @@ import { createUser } from "../services/users"
 
 function Users() {
     const [message, setMessage] = useState('')
+    const [errorMessage, setErrorMessage] = useState('')
     const create = useMutation({
         mutationFn: createUser,
         onSuccess: () => {
             setMessage('Account Created successfully')
             setTimeout(() => {
                 setMessage('')
-            },2000)
+            }, 2000)
+        },
+        onError: () => {
+            setErrorMessage('Unauthorized')
+            setTimeout(() => {
+                setErrorMessage('')
+            }, 3000)
         }
-    })    
+    })
     return (
         <div className="p-4 sm:ml-64 md:mt-14">
             <h1 className='block py-2.5 text-2xl px-0 w-full  text-gray-900  dark:text-gray-400  appearance-none pl-4 dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer'>Create user</h1>
             <div className="p-4 rounded-lg">
                 <Formik
-                    initialValues={{ name: '', salary: '' }}
+                    initialValues={{ name: '', lastname: '', salary: '', type: '', email: '', password: '', confirmPassword: '' }}
                     validate={values => {
                         const errors = {};
                         return errors;
                     }}
-                    onSubmit={(values, { resetForm }) => {                       
-                        create.mutate(values)
-                        resetForm()
-                    
+                    onSubmit={(values, { resetForm }) => {
+                        const user = {
+                            name: values.name,
+                            lastname: values.lastname,
+                            salary: values.salary,
+                            type: values.type,
+                            email: values.email,
+                            password: values.password
+                        }
+                        create.mutate(user)
                     }}
                 >
                     {({
@@ -42,27 +55,93 @@ function Users() {
                         <form method='POST' onSubmit={handleSubmit}>
                             <div className="relative z-0 w-full mb-6 group">
                                 <input type="text" name="name" id="name"
+                                    autoComplete='off'
                                     onChange={handleChange}
                                     onBlur={handleBlur}
                                     value={values.name} className="dark:text-white block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none  dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
                                 <label for="floating_email" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Name</label>
                             </div>
+                            <div class="relative z-0 w-full mb-6 group">
+                                <input
+                                    onChange={handleChange}
+                                    onBlur={handleBlur}
+                                    autoComplete='off'
+                                    value={values.lastname}
+                                    type="text" name="lastname" id="lastname" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                                <label for="floating_repeat_password" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Lastname</label>
+                            </div>
                             <div>
                                 <div className="relative z-0 w-full mb-6 group">
                                     <input type="number"
+                                        autoComplete='off'
                                         onChange={handleChange}
                                         onBlur={handleBlur}
                                         value={values.salary}
-                                        name="salary" id="salary" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 dark:text-white border-gray-300 appearance-none  dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                                        name="salary"
+                                        id="salary"
+                                        className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 dark:text-white border-gray-300 appearance-none  dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" "
+                                        required />
                                     <label for="floating_first_name" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Salary</label>
                                 </div>
                             </div>
-                            <button type="submit" className="text-white bg-indigo-400 hover:bg-indigo-500 transition-all focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-gray-600 dark:hover:bg-gray-700 dark:focus:bg-gray-900">Send</button>
+                            <div class="grid md:grid-cols-2 md:gap-6">
+                                <div class="relative z-0 w-full mb-6 group">
+                                    <input
+                                        type="number"
+                                        onChange={handleChange}
+                                        onBlur={handleBlur}
+                                        autoComplete='off'
+                                        value={values.type}
+                                        name="type" id="type" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                                    <label for="floating_first_name" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Type</label>
+                                </div>
+                                <div class="relative z-0 w-full mb-6 group">
+                                    <input
+                                        autoComplete='off'
+                                        onChange={handleChange}
+                                        onBlur={handleBlur}
+                                        value={values.email}
+                                        type="email"
+                                        name="email"
+                                        id="email"
+                                        class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" "
+                                        required />
+                                    <label for="floating_last_name" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Email</label>
+                                </div>
+                            </div>
+                            <div class="grid md:grid-cols-2 md:gap-6">
+                                <div class="relative z-0 w-full mb-6 group">
+                                    <input
+                                        autoComplete='off'
+                                        onChange={handleChange}
+                                        onBlur={handleBlur}
+                                        value={values.password}
+                                        type="password"
+                                        name="password"
+                                        id="password"
+                                        class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" "
+                                        required />
+                                    <label for="floating_phone" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Password</label>
+                                </div>
+                                <div class="relative z-0 w-full mb-6 group">
+                                    <input
+                                        autoComplete='off'
+                                        onChange={handleChange}
+                                        onBlur={handleBlur}
+                                        value={values.confirmPassword}
+                                        type="password" name="confirmPassword" id="confirmPassword" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                                    <label for="floating_company" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Confirm password</label>
+                                </div>
+                            </div>
+                            <button type="submit" className="text-white btn-submit transition-all focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center ">Send</button>
                             {
                                 message ? <div className='mt-5 text-green-700'>
                                     <span>{message}</span>
                                 </div> : <></>
                             }
+                            <div className="text-red-400 pt-5">
+                                {errorMessage}
+                            </div>
                         </form>
                     )}
                 </Formik>
