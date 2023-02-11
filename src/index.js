1e771bf0e5a56c0a2bd961eb5f01ce861a9d6f3a
iarmolla <ivan.armolla@gmail.com>
2023-02-11 18:21:10 -0300

    removing reactquerydevtools

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App"
 import { QueryClient, QueryClientProvider } from "react-query";
-import { ReactQueryDevtools } from 'react-query/devtools'
 
 const queryClient = new QueryClient()
 const root = ReactDOM.createRoot(document.getElementById("root"))
@@ -11,7 +10,6 @@ root.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
-      <ReactQueryDevtools></ReactQueryDevtools>
     </QueryClientProvider>
   </React.StrictMode>
 );
